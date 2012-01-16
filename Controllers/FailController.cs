d80952cc55cd17a9cfc789d1f3fc4151ad765bc7
Bertrand <Bertrand@Nutella>
2012-01-16 14:47:42 -0800

    Added Fail feature

---
+++ Controllers/FailController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Orchard.Environment.Extensions;
+using Orchard.Themes;
+
+namespace Vandelay.Industries.Controllers {
+    [Themed]
+    [OrchardFeature("Vandelay.Fail")]
+    public class FailController : Controller {
+        public ActionResult Index() {
+            throw new InvalidOperationException("Something went purposefully horribly wrong. Don't worry, it's going to be ok.");
+        }
+
+        public ActionResult NotFound() {
+            HttpContext.Response.StatusCode = 404;
+            return View();
+        }
+    }
+}
