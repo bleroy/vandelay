b8896aca61c0793737c62ca28b9aa90e899923d9
Zolt?n Leh?czky <lehoczky_zoltan@pyrocenter.hu>
2012-12-08 21:05:25 +0100

    #20: Removing forced overrides for error shapes Work item: 20

--- Controllers/FailController.cs
+++ Controllers/FailController.cs
@@ -7,18 +7,12 @@ namespace Vandelay.Industries.Controllers {
     [Themed]
     [OrchardFeature("Vandelay.Fail")]
     public class FailController : Controller {
-        public ActionResult Index() {
-            throw new InvalidOperationException("Something went purposefully horribly wrong. Don't worry, it's going to be ok.");
-        }
-
         public ActionResult NotFound() {
-            HttpContext.Response.StatusCode = 404;
-            return View("NotFound");
+            return HttpNotFound();
         }
 
         public ActionResult Error() {
-            HttpContext.Response.StatusCode = 500;
-            return View("ErrorPage");
+            throw new InvalidOperationException("Something went purposefully horribly wrong. Don't worry, it's going to be ok.");
         }
     }
 }
