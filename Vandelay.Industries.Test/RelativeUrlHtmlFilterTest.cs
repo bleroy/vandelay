852245efbcc1d181db962de46f8420bb0e969c05
Bertrand Le Roy <bertrandleroy@gmail.com>
2013-07-12 19:30:53 -0700

    Reverting 1.x API change

--- Vandelay.Industries.Test/RelativeUrlHtmlFilterTest.cs
+++ Vandelay.Industries.Test/RelativeUrlHtmlFilterTest.cs
@@ -18,28 +18,28 @@ namespace Vandelay.Industries.Test {
         [Test]
         public void RelativeImgSrceGetsReplaced() {
             const string html = @"<p><img src=""/foo/bar.png"" alt=""bar"">/foo/bar.png</p>";
-            var transformed = Filter.ProcessContent(html);
+            var transformed = Filter.ProcessContent(html, "html");
             Assert.AreEqual(@"<p><img src=""http://foo.com/foo/bar.png"" alt=""bar"">/foo/bar.png</p>", transformed);
         }
 
         [Test]
         public void AbsoluteImgSrcDoesNotGetReplaced() {
             const string html = @"<p><img src=""http://bar.com/foo/bar.png"" alt=""bar"">/foo/bar.png</p>";
-            var transformed = Filter.ProcessContent(html);
+            var transformed = Filter.ProcessContent(html, "html");
             Assert.AreEqual(@"<p><img src=""http://bar.com/foo/bar.png"" alt=""bar"">/foo/bar.png</p>", transformed);
         }
 
         [Test]
         public void RelativeLinkHrefGetsReplaced() {
             const string html = @"<p><a href=""/foo/bar"">/foo/bar</a></p>";
-            var transformed = Filter.ProcessContent(html);
+            var transformed = Filter.ProcessContent(html, "html");
             Assert.AreEqual(@"<p><a href=""http://foo.com/foo/bar"">/foo/bar</a></p>", transformed);
         }
 
         [Test]
         public void AbsoluteLinkHrefDoesNotGetReplaced() {
             const string html = @"<p><a href=""http://bar.com/foo/bar"">/foo/bar</a></p>";
-            var transformed = Filter.ProcessContent(html);
+            var transformed = Filter.ProcessContent(html, "html");
             Assert.AreEqual(@"<p><a href=""http://bar.com/foo/bar"">/foo/bar</a></p>", transformed);
         }
     }
