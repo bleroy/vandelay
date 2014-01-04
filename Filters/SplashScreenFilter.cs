d90c82ef1a9f6b6d8150874fe45c417aac567463
Bertrand Le Roy <bertrandleroy@gmail.com>
2014-01-04 15:58:41 -0800

    Don't splash screen the account controller (login, etc.), POST requests, and child actions. Added a setting for excluded URLs.

--- Filters/SplashScreenFilter.cs
+++ Filters/SplashScreenFilter.cs
@@ -1,17 +1,17 @@
 ﻿using System;
+using System.IO;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Orchard;
 using Orchard.ContentManagement;
 using Orchard.Environment.Extensions;
 using Orchard.Mvc.Filters;
+using Orchard.UI.Admin;
 using Vandelay.Industries.Models;
 
-namespace Vandelay.Industries.Filters
-{
+namespace Vandelay.Industries.Filters {
     [OrchardFeature("Vandelay.SplashScreen")]
-    public class SplashScreenFilter : FilterProvider, IResultFilter
-    {
+    public class SplashScreenFilter : FilterProvider, IResultFilter {
         public void OnResultExecuting(ResultExecutingContext filterContext) {
             // Splash screen itself is not subject to filtering
             var routeValues = filterContext.RouteData.Values;
@@ -20,14 +20,26 @@ namespace Vandelay.Industries.Filters
                 return;
             }
 
-            var httpContext = filterContext.HttpContext;
-            if (httpContext == null) return;
+            // Neither are requests to the account controller
+            if (routeValues["area"] as string == "Orchard.Users" &&
+                routeValues["controller"] as string == "Account") {
+                return;
+            }
 
-            // Don't filter on admin, media, or theme resources
+            var httpContext = filterContext.HttpContext;
+            if (httpContext == null || httpContext.Request == null) return;
+
+            // don't try to put a splash screen on POST requests
+            if (httpContext.Request.HttpMethod.Equals("POST", StringComparison.OrdinalIgnoreCase)) return;
+
+            // Don't put a splash screen on admin, media, or theme resources
             var requestedPath = httpContext.Request.AppRelativeCurrentExecutionFilePath;
             if (requestedPath.StartsWith("~/media/", StringComparison.OrdinalIgnoreCase)) return;
             if (requestedPath.StartsWith("~/themes/", StringComparison.OrdinalIgnoreCase)) return;
-            if (requestedPath.StartsWith("~/admin", StringComparison.OrdinalIgnoreCase)) return;
+            if (AdminFilter.IsApplied(new RequestContext(httpContext, new RouteData()))) return;
+
+            // ignore child actions, e.g. HomeController is using RenderAction()
+            if (filterContext.IsChildAction) return;
 
             var splashScreenCookie = httpContext.Request
                 .Cookies[SplashScreenSettingsPart.CookieName];
@@ -35,6 +47,11 @@ namespace Vandelay.Industries.Filters
             if (splashScreenCookie != null && !String.IsNullOrWhiteSpace(splashScreenCookie.Value)) return;
             var config = filterContext.GetWorkContext().CurrentSite.As<SplashScreenSettingsPart>();
             if (config == null || !config.Enabled) return;
+
+            // If request is for an ignored URL, don't display a splash screen
+            var ignoredUrls = config.IgnoredUrls;
+            if (IsIgnoredUrl(httpContext.Request.AppRelativeCurrentExecutionFilePath, ignoredUrls)) return;
+
             // This is a new user, and the splash screen is enabled. Redirect to splash screen.
             filterContext.Cancel = true;
             filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary {
@@ -46,8 +63,48 @@ namespace Vandelay.Industries.Filters
             filterContext.Result.ExecuteResult(filterContext.Controller.ControllerContext);
         }
 
-        public void OnResultExecuted(ResultExecutedContext filterContext)
-        {
+        public void OnResultExecuted(ResultExecutedContext filterContext) {}
+
+        /// <summary>
+        /// Returns true if the given url should be ignored, as defined in the settings
+        /// <remarks>Copied from OutputCacheFilter</remarks>
+        /// </summary>
+        private static bool IsIgnoredUrl(string url, string ignoredUrls) {
+            if (String.IsNullOrEmpty(ignoredUrls)) {
+                return false;
+            }
+
+            // remove ~ if present
+            if (url.StartsWith("~")) {
+                url = url.Substring(1);
+            }
+
+            using (var urlReader = new StringReader(ignoredUrls)) {
+                string relativePath;
+                while (null != (relativePath = urlReader.ReadLine())) {
+                    // remove ~ if present
+                    if (relativePath.StartsWith("~")) {
+                        relativePath = relativePath.Substring(1);
+                    }
+
+                    if (String.IsNullOrWhiteSpace(relativePath)) {
+                        continue;
+                    }
+
+                    relativePath = relativePath.Trim();
+
+                    // ignore comments
+                    if (relativePath.StartsWith("#")) {
+                        continue;
+                    }
+
+                    if (String.Equals(relativePath, url, StringComparison.OrdinalIgnoreCase)) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
